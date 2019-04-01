e3a6f6ce19868d9da1b24b866ba1861ce242f233
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2019-04-01 14:13:46 +0700

    revisi batasan login dengan is_active & is_deleted

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -9,6 +9,7 @@ use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Auth;
 
 class AuthController extends Controller
 {
