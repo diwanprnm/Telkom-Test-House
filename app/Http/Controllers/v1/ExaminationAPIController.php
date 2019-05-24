d281129cbd594ced82154eb76bd6a51da6ce6abd
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2019-05-24 13:23:27 +0700

    Perbaikan API function_test

--- app/Http/Controllers/v1/ExaminationAPIController.php
+++ app/Http/Controllers/v1/ExaminationAPIController.php
@@ -750,10 +750,10 @@ class ExaminationAPIController extends AppBaseController
 				$result = $result->where("devices.name", "LIKE", '%'.$param->device_name .'%');
 			}
 			if(isset($param->deal_test_date)){
-				/*$rawRangeDate ="date_format(examinations.deal_test_date,'%Y-%m-%d') = '".$param->deal_test_date."'";
-				$result = $result->where(\DB::raw($rawRangeDate), 1); */
-				$result = $result->where('examinations.function_test_date_approval', 1)
-				->where(DB::Raw('IFNULL( examinations.function_date, examinations.deal_test_date ) = "'.$param->deal_test_date.'"'));
+				$rawRangeDate ="date_format(examinations.deal_test_date,'%Y-%m-%d') = '".$param->deal_test_date."'";
+				$result = $result->where(\DB::raw($rawRangeDate), 1); 
+				/*$result = $result->where('examinations.function_test_date_approval', 1)
+				->where(DB::Raw('IFNULL( examinations.function_date, examinations.deal_test_date ) = "'.$param->deal_test_date.'"'));*/
 			}
 			if(isset($param->function_result)){
 				$result = $result->where("examinations.function_test_TE", "=", $param->function_result);
