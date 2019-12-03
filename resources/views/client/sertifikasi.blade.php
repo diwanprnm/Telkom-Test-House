830c8daad85ae2bf510f2103acb27c52dec3c6a5
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2019-12-03 21:32:03 +0700

    sertifikasi

--- resources/views/client/sertifikasi.blade.php
+++ resources/views/client/sertifikasi.blade.php
@@ -27,23 +27,19 @@
 
 				<div class="container clearfix">
 
-					<!-- <div class="col_full">
+					<div class="col_full">
 						@if( Config::get('app.locale') == 'in')
-							<?php /*if(count($data)==0){
-								echo "Data Not Found";
-							}else{
+							<?php if(count($data)>0){
 								echo strip_tags($data[0]->description);
-							}*/
+							}
 							?>
 						@else
-							<?php /*if(count($data)==0){
-								echo "Data Not Found";
-							}else{
+							<?php if(count($data)>0){
 								echo strip_tags($data[0]->description_english);
-							}*/
+							}
 							?>
 						@endif
-					</div> -->
+					</div>
 
 					<div class="col_full">
 
