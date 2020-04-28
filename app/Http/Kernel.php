badf0c6c2c889d3a9b1b1e72fc130ae734fe6baf
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2020-04-28 17:15:40 +0700

    provider http -> https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,9 +15,6 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-
-        // appending custom middleware 
-        'MyApp\Http\Middleware\HttpsProtocol'
     ];
 
     /**
