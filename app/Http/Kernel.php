6d69fe2b5e5f432321a2658f595af6b21e8ce8dd
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2020-04-28 18:44:18 +0700

    force http -> https

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -31,7 +31,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
 			\App\Http\Middleware\App::class,
 			\PragmaRX\Tracker\Vendor\Laravel\Middlewares\Tracker::class,
-            // \App\Http\Middleware\HttpsProtocol::class,
         ],
 		
         'api' => [
