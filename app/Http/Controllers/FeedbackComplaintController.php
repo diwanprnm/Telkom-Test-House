0be9bbd47ae68f89980b9a0472e561aff5805aab
daniel_ubuntu <danielchristianto77@gmail.com>
2020-06-28 10:10:49 +0700

    Admin/FeedbackAndComplain

--- app/Http/Controllers/FeedbackComplaintController.php
+++ app/Http/Controllers/FeedbackComplaintController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
 use Auth;
@@ -24,6 +23,12 @@ use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
 
 class FeedbackComplaintController extends Controller
 {
+    private const SEARCH = 'search';
+    private const EXAMINATION_DEVICE = 'examination.device';
+    private const RECAP_FEEDBACK_COMPLAINT = "Rekap Feedback dan Complaint";
+    private const SORT_BY = 'sort_by';
+    private const SORT_TYPE = 'sort_type';
+    private const EXAMINATION_COMPANY = 'examination.company';
     /**
      * Create a new controller instance.
      *
@@ -46,7 +51,7 @@ class FeedbackComplaintController extends Controller
         if ($currentUser){
             $message = null;
             $paginate = 10;
-            $search = trim($request->input('search'));
+            $search = trim($request->input(self::SEARCH));
 
             $before = null;
             $after = null;
@@ -61,85 +66,30 @@ class FeedbackComplaintController extends Controller
             $companies = Company::where('id','!=', 1)->get();
             $examLab = ExaminationLab::all();
 
-            $query = Questioner::with('examination.device')->with('examination.company')->with('user');
-
-            $questioner = $query->get();
+            $query = Questioner::with(self::EXAMINATION_DEVICE)->with(self::EXAMINATION_COMPANY)->with('user');
             
             if ($search != null){
-                    $query->whereHas('examination.company', function ($query) use ($search) {
+                    $query->whereHas(self::EXAMINATION_COMPANY, function ($query) use ($search) {
                         $query->where('name', 'like', '%'.strtolower($search).'%');
-                    })->orWhereHas('examination.device', function ($query) use ($search) {
+                    })->orWhereHas(self::EXAMINATION_DEVICE, function ($query) use ($search) {
                         $query->orWhere('name', 'like', '%'.strtolower($search).'%');
                     });
 
                 $logs = new Logs;
                 $logs->user_id = $currentUser->id;$logs->id = Uuid::uuid4();
                 $logs->action = "search";  
-                $dataSearch = array('search' => $search);
+                $dataSearch = array(self::SEARCH => $search);
                 $logs->data = json_encode($dataSearch);
                 $logs->created_by = $currentUser->id;
-                $logs->page = "Rekap Feedback dan Complaint";
+                $logs->page = self::RECAP_FEEDBACK_COMPLAINT;
                 $logs->save();
             }
-            
-            /*if ($request->has('before_date')){
-                $query->where(DB::raw('(
-                    SELECT
-                        action_date AS tgl_masuk_gudang
-                    FROM
-                        equipment_histories
-                    WHERE
-                        examination_id = examinations.id
-                    AND location = 2
-                    ORDER BY
-                        created_at ASC
-                    LIMIT 1
-                )'), '<=', $request->get('before_date'));
-                $before = $request->get('before_date');
-            }
 
-            if ($request->has('after_date')){
-                $query->where(DB::raw('(
-                    SELECT
-                        action_date AS tgl_masuk_gudang
-                    FROM
-                        equipment_histories
-                    WHERE
-                        examination_id = examinations.id
-                    AND location = 2
-                    ORDER BY
-                        created_at ASC
-                    LIMIT 1
-                )'), '>=', $request->get('after_date'));
-                $after = $request->get('after_date');
+            if ($request->has(self::SORT_BY)){
+                $sort_by = $request->get(self::SORT_BY);
             }
-
-            if ($request->has('type')){
-                $type = $request->get('type');
-                if($request->input('type') != 'all'){
-                    $query->where('examination_type_id', $request->get('type'));
-                }
-            }
-
-            if ($request->has('company')){
-                $filterCompany = $request->get('company');
-                if($request->input('company') != 'all'){
-                    $query->where('companies.name', $request->get('company'));
-                }
-            }
-
-            if ($request->has('lab')){
-                $lab = $request->get('lab');
-                if($request->input('lab') != 'all'){
-                    $query->where('examination_lab_id', $request->get('lab'));
-                }
-            }
-*/
-            if ($request->has('sort_by')){
-                $sort_by = $request->get('sort_by');
-            }
-            if ($request->has('sort_type')){
-                $sort_type = $request->get('sort_type');
+            if ($request->has(self::SORT_TYPE)){
+                $sort_type = $request->get(self::SORT_TYPE);
             }
 
             $data = $query->orderBy($sort_by, $sort_type)
@@ -152,7 +102,7 @@ class FeedbackComplaintController extends Controller
             return view('admin.feedbackncomplaint.index')
                 ->with('message', $message)
                 ->with('data', $data)
-                ->with('search', $search)
+                ->with(self::SEARCH, $search)
                 ->with('before_date', $before)
                 ->with('after_date', $after)
                 ->with('type', $examType)
@@ -161,8 +111,8 @@ class FeedbackComplaintController extends Controller
                 ->with('filterCompany', $filterCompany)
                 ->with('lab', $examLab)
                 ->with('filterLab', $lab)
-                ->with('sort_by', $sort_by)
-                ->with('sort_type', $sort_type);
+                ->with(self::SORT_BY, $sort_by)
+                ->with(self::SORT_TYPE, $sort_type);
         }
     }
 
@@ -174,34 +124,25 @@ class FeedbackComplaintController extends Controller
         // the user's e-mail address, the amount paid, and the payment
         // timestamp.
 
-        $search = trim($request->input('search'));
-
-        $before = null;
-        $after = null;
-        $type = '';
-        $filterCompany = '';
-        $lab = '';
-
+        $search = trim($request->input(self::SEARCH));
         $sort_by = 'questioner_date';
         $sort_type = 'desc';
 
-        $query = Questioner::with('examination.device')->with('examination.company')->with('examination.questionerdynamic')->with('examination.examinationType')->with('user');
-
-        $questioner = $query->get();
+        $query = Questioner::with(self::EXAMINATION_DEVICE)->with(self::EXAMINATION_COMPANY)->with('examination.questionerdynamic')->with('examination.examinationType')->with('user');
         
         if ($search != null){
-            $query->whereHas('examination.company', function ($query) use ($search) {
+            $query->whereHas(self::EXAMINATION_COMPANY, function ($query) use ($search) {
                 $query->where('name', 'like', '%'.strtolower($search).'%');
-            })->orWhereHas('examination.device', function ($query) use ($search) {
+            })->orWhereHas(self::EXAMINATION_DEVICE, function ($query) use ($search) {
                 $query->orWhere('name', 'like', '%'.strtolower($search).'%');
             });
         }
 
-        if ($request->has('sort_by')){
-            $sort_by = $request->get('sort_by');
+        if ($request->has(self::SORT_BY)){
+            $sort_by = $request->get(self::SORT_BY);
         }
-        if ($request->has('sort_type')){
-            $sort_type = $request->get('sort_type');
+        if ($request->has(self::SORT_TYPE)){
+            $sort_type = $request->get(self::SORT_TYPE);
         }
 
         $data = $query->orderBy($sort_by, $sort_type)->get();
@@ -221,11 +162,12 @@ class FeedbackComplaintController extends Controller
 
         $merge_value = 0;
         for ($i=0; $i<count($data[0]->examination->questionerdynamic); $i++) {
-            if ($i>0) {
-                $data[0]->examination->questionerdynamic[$i]->is_essay == 0 ? array_push($examsArray[0], '') : '';
-                $merge_value++;
-            } 
+            if ($i==0) {continue;}
+
+            $data[0]->examination->questionerdynamic[$i]->is_essay == 0 ? array_push($examsArray[0], '') : '';
+            $merge_value++;
         }
+        
         array_push($examsArray[0], 'Tingkat Kepuasan');
         $examsArray[1] = [];
         array_push($examsArray[1], 
@@ -246,13 +188,13 @@ class FeedbackComplaintController extends Controller
         // and append it to the payments array.
         $no = 3;
         foreach ($data as $row) {
-            $questioner_date = !isset($row->questioner_date) ? '' : $row->questioner_date;
-            $user_name = !isset($row->user->name) ? '' : $row->user->name;
-            $company_name = !isset($row->examination->company->name) ? '' : $row->examination->company->name;
-            $phone_number = !isset($row->user->phone_number) ? '' : $row->user->phone_number;
-            $examType_name = !isset($row->examination->examinationType->name) ? '' : $row->examination->examinationType->name;
-            $examType_desc = !isset($row->examination->examinationType->description) ? '' : $row->examination->examinationType->description;
-            $device_name = !isset($row->examination->device->name) ? '' : $row->examination->device->name;
+            $questioner_date = $this->filterDefault($row->questioner_date);
+            $user_name = $this->filterDefault($row->user->name);
+            $company_name = $this->filterDefault($row->examination->company->name);
+            $phone_number = $this->filterDefault($row->user->phone_number);
+            $examType_name = $this->filterDefault($row->examination->examinationType->name);
+            $examType_desc = $this->filterDefault($row->examination->examinationType->description);
+            $device_name = $this->filterDefault($row->examination->device->name);
                         
             $examsArray[] = [
                 $no-2,
@@ -264,33 +206,26 @@ class FeedbackComplaintController extends Controller
                 $device_name
             ];
             foreach ($row->examination->questionerdynamic as $questioner_value) {
-                $questioner_value->is_essay == 0 ? array_push($examsArray[$no-1], $questioner_value->eks_answer) : '';
-            }
-            foreach ($row->examination->questionerdynamic as $questioner_value) {
-                $questioner_value->is_essay == 0 ? array_push($examsArray[$no-1], $questioner_value->perf_answer) : '';
+                $questioner_value->is_essay == 0 ? array_push($examsArray[$no-1], $questioner_value->eks_answer) && array_push($examsArray[$no-1], $questioner_value->perf_answer) : '';
             }
             $no++;
         }
+
         $currentUser = Auth::user();
         $logs = new Logs;
-        $logs->user_id = $currentUser->id;$logs->id = Uuid::uuid4();
+        $logs->id = Uuid::uuid4();
+        $logs->user_id = $currentUser->id;
         $logs->action = "download_excel";   
         $logs->data = "";
         $logs->created_by = $currentUser->id;
-        $logs->page = "Rekap Feedback dan Complaint";
+        $logs->page = self::RECAP_FEEDBACK_COMPLAINT;
         $logs->save();
 
         // Generate and return the spreadsheet
-        Excel::create('Data Feedback', function($excel) use ($examsArray, $merge_value) {
-
-            // Set the spreadsheet title, creator, and description
-            // $excel->setTitle('Payments');
-            // $excel->setCreator('Laravel')->setCompany('WJ Gilmore, LLC');
-            // $excel->setDescription('payments file');
+        Excel::create('Data Feedback', function($excel) use ($examsArray) {
 
             // Build the spreadsheet, passing in the payments array
-            $excel->sheet('sheet1', function($sheet) use ($examsArray, $merge_value) {
-                // $alphabet = range('H', 'Z');
+            $excel->sheet('sheet1', function($sheet) use ($examsArray) {
                 $sheet->mergeCells('A1:A2');
                 $sheet->mergeCells('B1:B2');
                 $sheet->mergeCells('C1:C2');
@@ -298,11 +233,16 @@ class FeedbackComplaintController extends Controller
                 $sheet->mergeCells('E1:E2');
                 $sheet->mergeCells('F1:F2');
                 $sheet->mergeCells('G1:G2');
-                // $s = $alphabet[$merge_value];
                 $sheet->mergeCells('H1:AF1');
                 $sheet->mergeCells('AG1:BE1');
                 $sheet->fromArray($examsArray, null, 'A1', false, false);
             });
         })->export('xlsx'); 
+    }
+
+    private function filterDefault($string, $is_number = false)
+    {
+        $is_number? $defaultValue = '' : '0';
+        return isset($string)? $string : $defaultValue ;
     }
 }
--- resources/views/admin/feedbackncomplaint/index.blade.php
+++ resources/views/admin/feedbackncomplaint/index.blade.php
@@ -29,7 +29,6 @@
 		<div class="container-fluid container-fullw bg-white">
 	        <div class="row">
 	        	<div class="col-md-6">
-	    			<!-- <a class="btn btn-wide btn-primary pull-left" data-toggle="collapse" href="#collapse1" style="margin-right: 10px;"><i class="ti-filter"></i> Filter</a> -->
 	    			<button id="excel" type="submit" class="btn btn-info pull-left">
                         Export to Excel
                     </button>
@@ -37,7 +36,7 @@
 				<div class="col-md-6">
 	                <span class="input-icon input-icon-right search-table">
 	                    <input id="search_value" type="text" placeholder="Search" id="form-field-17" class="form-control " value="{{ $search }}">
-	                    <i class="ti-search"></i>
+	                    <em class="ti-search"></em>
 	                </span>
 	            </div>
 	            <div class="col-md-12 panel panel-info">
@@ -56,7 +55,7 @@
 											<input type="text" placeholder="Dari Tanggal" value="{{ $after_date }}" name="after_date" id="after_date" class="form-control"/>
 											<span class="input-group-btn">
 												<button type="button" class="btn btn-default">
-													<i class="glyphicon glyphicon-calendar"></i>
+													<em class="glyphicon glyphicon-calendar"></em>
 												</button>
 											</span>
 										</p>
@@ -71,7 +70,7 @@
 											<input type="text" placeholder="Sampai Tanggal" value="{{ $before_date }}" name="before_date" id="before_date" class="form-control"/>
 											<span class="input-group-btn">
 												<button type="button" class="btn btn-default">
-													<i class="glyphicon glyphicon-calendar"></i>
+													<em class="glyphicon glyphicon-calendar"></em>
 												</button>
 											</span>
 										</p>
@@ -153,62 +152,6 @@
 										</select>
 									</div>
 								</div>
-
-								<!-- sorting  -->
-								<!-- <div class="col-md-6">
-									<div class="form-group">
-										<label>
-											Sort by :
-										</label>
-										<select id="sort_by" name="sort_by" class="cs-select cs-skin-elastic" required>
-											@if($sort_by == '')
-												<option value="" disabled selected>Select...</option>
-											@endif
-											@if($sort_by == 'tgl_masuk_barang')
-                                                <option value="tgl_masuk_barang" selected>Tanggal Masuk Barang</option>
-											@else
-                                                <option value="tgl_masuk_barang">Tanggal Masuk Barang</option>
-                                            @endif
-											@if($sort_by == 'no')
-                                                <option value="no" selected>Feedback dan Complaint</option>
-											@else
-                                                <option value="no">Feedback dan Complaint</option>
-                                            @endif
-                                            @if($sort_by == 'examination_type_id')
-                                                <option value="examination_type_id" selected>Tipe Pengujian</option>
-											@else
-                                                <option value="examination_type_id">Tipe Pengujian</option>
-                                            @endif
-											@if($sort_by == 'company_name')
-                                                <option value="company_name" selected>Nama Perusahaan</option>
-											@else
-                                                <option value="company_name">Nama Perusahaan</option>
-                                            @endif
-										</select>
-										<select id="sort_type" name="sort_type" class="cs-select cs-skin-elastic" required>
-											@if($sort_type == '')
-												<option value="" disabled selected>Select...</option>
-											@endif
-											@if($sort_type == 'asc')
-                                                <option value="asc" selected>ASC</option>
-											@else
-                                                <option value="asc">ASC</option>
-                                            @endif
-											@if($sort_type == 'desc')
-                                                <option value="desc" selected>DESC</option>
-											@else
-                                                <option value="desc">DESC</option>
-                                            @endif
-										</select>
-									</div>
-								</div> -->
-								<!-- end sorting  -->
-
-								<!-- <div class="col-md-12">
-		                            <button id="filter" type="submit" class="btn btn-wide btn-green btn-squared pull-right">
-		                                Filter
-		                            </button>
-		                        </div> -->
 		                    </div>
 						</fieldset>
 			    	</div>
@@ -231,15 +174,16 @@
 				<div class="col-md-12">
 					<div class="table-responsive">
 						<table class="table table-striped table-bordered table-hover table-full-width dataTable no-footer">
+							<caption>Feedback and Complain Score Table</caption>
 							<thead>
 								<tr>
-									<th class="center">No</th>
-									<th class="center">Nama</th>
-                                    <th class="center">Perusahaan</th>
-                                    <th class="center">Perangkat</th>
-									<th class="center">Tipe</th>
-									<th class="center">Kapasitas</th>
-									<th class="center">Aksi</th>
+									<th class="center" scope="col">No</th>
+									<th class="center" scope="col">Nama</th>
+                                    <th class="center" scope="col">Perusahaan</th>
+                                    <th class="center" scope="col">Perangkat</th>
+									<th class="center" scope="col">Tipe</th>
+									<th class="center" scope="col">Kapasitas</th>
+									<th class="center" scope="col">Aksi</th>
 								</tr>
 							</thead>
 							<tbody>
@@ -307,116 +251,16 @@
 	            var baseUrl = "{{URL::to('/')}}";
 				var params = {
 					search:document.getElementById("search_value").value,
-					/*type:document.getElementById("type").value,
-					after_date:document.getElementById("after_date").value,
-					before_date:document.getElementById("before_date").value,
-					company:document.getElementById("company").value,
-					lab:document.getElementById("lab").value,*/
-/*sorting*/
-					/*sort_by:document.getElementById("sort_by").value,
-					sort_type:document.getElementById("sort_type").value*/
-/*end sorting*/
 				};
 				document.location.href = baseUrl+'/admin/feedbackncomplaint?'+jQuery.param(params);
 	        }
 	    });
-/*
-	    document.getElementById("filter").onclick = function() {
-            var baseUrl = "{{URL::to('/')}}";
-            var params = {};
-			var search_value = document.getElementById("search_value").value;
-			var before = document.getElementById("before_date");
-            var after = document.getElementById("after_date");
-			var beforeValue = before.value;
-			var afterValue = after.value;
-            var type = document.getElementById("type");
-			var typeValue = type.options[type.selectedIndex].value;
-			var company = document.getElementById("company");
-			var companyValue = company.options[company.selectedIndex].value;
-			var lab = document.getElementById("lab");
-			var labValue = lab.options[lab.selectedIndex].value;*/
-/*sorting*/
-			/*var sort_by = document.getElementById("sort_by");
-			var sort_byValue = sort_by.options[sort_by.selectedIndex].value;
-			var sort_type = document.getElementById("sort_type");
-			var sort_typeValue = sort_type.options[sort_type.selectedIndex].value;*/
-/*end sorting*/
-
-			/*params['search'] = search_value;
-			
-			if (beforeValue != ''){
-				params['before_date'] = beforeValue;
-			}
-			if (afterValue != ''){
-				params['after_date'] = afterValue;
-			}
-			if (typeValue != ''){
-				params['type'] = typeValue;
-			}
-			if (companyValue != ''){
-				params['company'] = companyValue;
-			}
-			if (labValue != ''){
-				params['lab'] = labValue;
-			}*/
-/*sorting*/			
-			/*if (sort_byValue != ''){
-				params['sort_by'] = sort_byValue;
-			}
-			if (sort_typeValue != ''){
-				params['sort_type'] = sort_typeValue;
-			}*/
-/*end sorting*/			
-/*			document.location.href = baseUrl+'/admin/feedbackncomplaint?'+jQuery.param(params);
-	    };*/
 
 	    document.getElementById("excel").onclick = function() {
             var baseUrl = "{{URL::to('/')}}";
             var params = {};
 			var search_value = document.getElementById("search_value").value;
-			/*var before = document.getElementById("before_date");
-            var after = document.getElementById("after_date");
-			var beforeValue = before.value;
-			var afterValue = after.value;
-            var type = document.getElementById("type");
-			var typeValue = type.options[type.selectedIndex].value;
-			var company = document.getElementById("company");
-			var companyValue = company.options[company.selectedIndex].value;
-			var lab = document.getElementById("lab");
-			var labValue = lab.options[lab.selectedIndex].value;*/
-			
-/*sorting*/
-			/*var sort_by = document.getElementById("sort_by");
-			var sort_byValue = sort_by.options[sort_by.selectedIndex].value;
-			var sort_type = document.getElementById("sort_type");
-			var sort_typeValue = sort_type.options[sort_type.selectedIndex].value;*/
-/*end sorting*/
-
-			params['search'] = search_value;
-			
-			/*if (beforeValue != ''){
-				params['before_date'] = beforeValue;
-			}
-			if (afterValue != ''){
-				params['after_date'] = afterValue;
-			}
-			if (typeValue != ''){
-				params['type'] = typeValue;
-			}
-			if (companyValue != ''){
-				params['company'] = companyValue;
-			}
-			if (labValue != ''){
-				params['lab'] = labValue;
-			}*/
-/*sorting*/			
-			/*if (sort_byValue != ''){
-				params['sort_by'] = sort_byValue;
-			}
-			if (sort_typeValue != ''){
-				params['sort_type'] = sort_typeValue;
-			}*/
-/*end sorting*/			
+			params['search'] = search_value;			
 			document.location.href = baseUrl+'/feedbackncomplaint/excel?'+jQuery.param(params);
 	    };
 	});
