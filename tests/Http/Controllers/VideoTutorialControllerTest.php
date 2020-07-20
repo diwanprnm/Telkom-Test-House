b8c5272d62d340575130ddfedd715623ff2f2b04
Loxa <michaeladreil5@gmail.com>
2020-07-20 19:40:23 +0700

    unit test video controler

--- tests/Http/Controllers/VideoTutorialControllerTest.php
+++ tests/Http/Controllers/VideoTutorialControllerTest.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use app\videoTutorial;
+use App\User;
 class VideoTutorialControllerTest extends TestCase
 {
     /**
