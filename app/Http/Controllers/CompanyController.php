48cca7d6291bc4fb83888c189219f56f1786239d
darulfazhal <fazhaldarul@gmail.com>
2020-08-11 12:44:17 +0700

    fixed unittesting

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -526,7 +526,7 @@ class CompanyController extends Controller
         if ($company){
             switch ($name) {
                 case 'npwp': 
-                    $file = Storage::disk(self::MINIO)->url('company/'.$company->npwp_file);
+                    $file = Storage::disk(self::MINIO)->url('company/'.$id."/".$company->npwp_file);
                      
                     $filename = $company->npwp_file;
                     $tempImage = tempnam(sys_get_temp_dir(), $filename);
@@ -536,7 +536,7 @@ class CompanyController extends Controller
                     break;
 
                 case 'siup':  
-                    $file = Storage::disk(self::MINIO)->url('company/'.$company->siup_file);
+                    $file = Storage::disk(self::MINIO)->url('company/'.$id."/".$company->siup_file);
                      
                     $filename = $company->npwp_file;
                     $tempImage = tempnam(sys_get_temp_dir(), $filename);
@@ -547,7 +547,7 @@ class CompanyController extends Controller
 
                 case 'qs': 
 
-                    $file = Storage::disk(self::MINIO)->url('company/'.$company->qs_certificate_file);
+                    $file = Storage::disk(self::MINIO)->url('company/'.$id."/".$company->qs_certificate_file);
                      
                     $filename = $company->qs_certificate_file;
                     $tempImage = tempnam(sys_get_temp_dir(), $filename);
--- tests/Http/Controllers/CompanyControllerTest.php
+++ tests/Http/Controllers/CompanyControllerTest.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions; 
 use App\Company;  
+use App\User;  
 
 class CompanyControllerTest extends TestCase
 {
@@ -14,89 +15,89 @@ class CompanyControllerTest extends TestCase
      * @return void
      */
 
-	public function testDeleteSoon(){
-        $this->assertTrue(true);
-    }
+	// public function testDeleteSoon(){
+ //        $this->assertTrue(true);
+ //    }
 
-    // public function test_visit_company()
-	// { 
-	//    $response = $this->call('GET', 'admin/company');  
-    //    $this->assertEquals(200, $response->status());
-	// }
-	// public function test_search_company()
-	// { 
-	//    $response = $this->call('GET', 'admin/company?search=asda&is_active=&after_date=&before_date=');  
-    //    $this->assertEquals(200, $response->status());
-	// }
-    // public function test_stores_company()
-	// { 
-	// 	$user = factory(App\User::class)->create(); 
+    public function test_visit_company()
+	{ 
+	   $response = $this->call('GET', 'admin/company');  
+       $this->assertEquals(200, $response->status());
+	}
+	public function test_search_company()
+	{ 
+	   $response = $this->call('GET', 'admin/company?search=asda&is_active=&after_date=&before_date=');  
+       $this->assertEquals(200, $response->status());
+	}
+    public function test_stores_company()
+	{ 
+		$user = User::find(1);
        
-	// 	$response =  $this->actingAs($user)->call('POST', 'admin/company', 
-	// 	[ 
-	//         'name' => str_random(10),
-	//         'address' => str_random(10),
-	//         'city' => str_random(10) ,
-	//         'email' =>  str_random(10),
-	//         'postal_code' => str_random(10) ,
-	//         'phone_number' =>mt_rand(0,10000) ,
-	//         'fax' => str_random(10) ,
-	//         'npwp_number' => mt_rand(0,10000) ,
-	//         'npwp_file' => str_random(10) ,
-	//         'siup_number' => str_random(10) ,
-	//         'siup_file' => str_random(10), 
-	//         'siup_date' => str_random(10), 
-	//         'qs_certificate_number' => str_random(10) ,
-	//         'qs_certificate_file' => str_random(10) ,
-	//         'qs_certificate_date' => str_random(10), 
-	//         'is_active' => mt_rand(0,1),
-	//         'created_by' => mt_rand(0,1), 
-	//         'updated_by' => mt_rand(0,1), 
-	//         'plg_id' => mt_rand(0,10),
-	//         'nib' => mt_rand(0,1)
-	//     ]);   
-	// 	// dd($response->getContent());
-    //     $this->assertEquals(302, $response->status());
-	//     // $company = factory(App\Company::class)->make();  
-	// }
-    // public function test_update_company()
-	// { 
-	// 	$user = factory(App\User::class)->create(); 
-    //    	$company = Company::latest()->first();
-	// 	$response =  $this->actingAs($user)->call('PUT', 'admin/company/'.$company->id, 
-	// 	[ 
-	//         'name' => str_random(10),
-	//         'address' => str_random(10),
-	//         'city' => str_random(10) ,
-	//         'email' =>  str_random(10),
-	//         'postal_code' => str_random(10) ,
-	//         'phone_number' =>mt_rand(0,10000) ,
-	//         'fax' => str_random(10) ,
-	//         'npwp_number' => mt_rand(0,10000) ,
-	//         'npwp_file' => str_random(10) ,
-	//         'siup_number' => str_random(10) ,
-	//         'siup_file' => str_random(10), 
-	//         'siup_date' => str_random(10), 
-	//         'qs_certificate_number' => str_random(10) ,
-	//         'qs_certificate_file' => str_random(10) ,
-	//         'qs_certificate_date' => str_random(10), 
-	//         'is_active' => mt_rand(0,1),
-	//         'created_by' => mt_rand(0,1), 
-	//         'updated_by' => mt_rand(0,1), 
-	//         'plg_id' => mt_rand(0,10),
-	//         'nib' => mt_rand(0,1)
-	//     ]);   
-	// 	// dd($response->getContent());
-    //     $this->assertEquals(302, $response->status());
-	//     // $company = factory(App\Company::class)->make();  
-	// }
-    // public function test_delete_company()
-	// { 
-	// 	$user = factory(App\User::class)->create(); 
-    //    	$company = Company::latest()->first();
-	// 	$response =  $this->actingAs($user)->call('DELETE', 'admin/company/'.$company->id);   
-	// 	// dd($response->getContent());
-    //     $this->assertEquals(302, $response->status());
-	//     // $company = factory(App\Company::class)->make();  
-	// }
+		$response =  $this->actingAs($user)->call('POST', 'admin/company', 
+		[ 
+	        'name' => str_random(10),
+	        'address' => str_random(10),
+	        'city' => str_random(10) ,
+	        'email' =>  str_random(10),
+	        'postal_code' => str_random(10) ,
+	        'phone_number' =>mt_rand(0,10000) ,
+	        'fax' => str_random(10) ,
+	        'npwp_number' => mt_rand(0,10000) ,
+	        'npwp_file' => str_random(10) ,
+	        'siup_number' => str_random(10) ,
+	        'siup_file' => str_random(10), 
+	        'siup_date' => str_random(10), 
+	        'qs_certificate_number' => str_random(10) ,
+	        'qs_certificate_file' => str_random(10) ,
+	        'qs_certificate_date' => str_random(10), 
+	        'is_active' => mt_rand(0,1),
+	        'created_by' => mt_rand(0,1), 
+	        'updated_by' => mt_rand(0,1), 
+	        'plg_id' => mt_rand(0,10),
+	        'nib' => mt_rand(0,1)
+	    ]);   
+		// dd($response->getContent());
+        $this->assertEquals(302, $response->status());
+	    // $company = factory(App\Company::class)->make();  
+	}
+    public function test_update_company()
+	{ 
+		$user = factory(App\User::class)->create(); 
+       	$company = Company::latest()->first();
+		$response =  $this->actingAs($user)->call('PUT', 'admin/company/'.$company->id, 
+		[ 
+	        'name' => str_random(10),
+	        'address' => str_random(10),
+	        'city' => str_random(10) ,
+	        'email' =>  str_random(10),
+	        'postal_code' => str_random(10) ,
+	        'phone_number' =>mt_rand(0,10000) ,
+	        'fax' => str_random(10) ,
+	        'npwp_number' => mt_rand(0,10000) ,
+	        'npwp_file' => str_random(10) ,
+	        'siup_number' => str_random(10) ,
+	        'siup_file' => str_random(10), 
+	        'siup_date' => str_random(10), 
+	        'qs_certificate_number' => str_random(10) ,
+	        'qs_certificate_file' => str_random(10) ,
+	        'qs_certificate_date' => str_random(10), 
+	        'is_active' => mt_rand(0,1),
+	        'created_by' => mt_rand(0,1), 
+	        'updated_by' => mt_rand(0,1), 
+	        'plg_id' => mt_rand(0,10),
+	        'nib' => mt_rand(0,1)
+	    ]);   
+		// dd($response->getContent());
+        $this->assertEquals(302, $response->status());
+	    // $company = factory(App\Company::class)->make();  
+	}
+    public function test_delete_company()
+	{ 
+		$user = factory(App\User::class)->create(); 
+       	$company = Company::latest()->first();
+		$response =  $this->actingAs($user)->call('DELETE', 'admin/company/'.$company->id);   
+		// dd($response->getContent());
+        $this->assertEquals(302, $response->status());
+	    // $company = factory(App\Company::class)->make();  
+	}
 }
