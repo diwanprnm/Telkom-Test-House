b08aee3d70893d3436e7b4cbb0fddfa2d8dea05e
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2020-08-25 21:13:27 +0700

    test ExaminatonLab

--- tests/Http/Controllers/ExaminationLabControllerTest.php
+++ tests/Http/Controllers/ExaminationLabControllerTest.php
@@ -15,72 +15,69 @@ class ExaminationLabControllerTest extends TestCase
      *
      * @return void
      */
-
-	public function testDeleteSoon(){
-        $this->assertTrue(true);
-    }
-
-     public function test_visit()
-	 { 
+	public function test_visit()
+	{ 
 		$admin = User::find('1');
-	    $response = $this->actingAs($admin)->call('GET', 'admin/labs');  
-        $this->assertEquals(200, $response->status());
-	 }
-	 public function test_search()
-	 { $admin = User::find('1');
+		$response = $this->actingAs($admin)->call('GET', 'admin/labs');  
+		$this->assertEquals(200, $response->status());
+	}
+	
+	public function test_search()
+	{ 
+		$admin = User::find('1');
 		$response = $this->actingAs($admin)->call('GET', 'admin/labs?search=asda');  
-        $this->assertEquals(200, $response->status());
-	 }
-	/* public function test_create()
-	 { 
+		$this->assertEquals(200, $response->status());
+	}
+
+	public function test_create()
+	{ 
 		$admin = User::find('1');
-	    $response = $this->actingAs($admin)->call('GET', 'admin/labs/create');  
-        $this->assertEquals(200, $response->status());
-	 }
-     public function test_stores()
-	 { 
-	 	
+		$response = $this->actingAs($admin)->call('GET', 'admin/labs/create');  
+		$this->assertEquals(200, $response->status());
+	}
+
+	public function test_stores()
+	{ 
 		$admin = User::find('1');
 		$response = $this->actingAs($admin)->call('POST', 'admin/labs', 
-	 	[ 
-	         'name' => str_random(10),
-	         'lab_code' => str_random(10),
-             'lab_init' => str_random(10) , 
-             'description' => str_random(10) ,
-	         'is_active' => mt_rand(0,1)
-	        
-		 ]);   
-         $this->assertEquals(302, $response->status());
-	      
-	 }
-	 public function test_edit()
-	 { 
-		$admin = User::find('1');
-	    $response = $this->actingAs($admin)->call('GET', 'admin/labs/edit');  
-        $this->assertEquals(200, $response->status());
-	 }
-     public function test_update()
-	 { 
-	 	$user = factory(App\User::class)->create(); 
-        	$lab = ExaminationLab::latest()->first();
-	 	$response =  $this->actingAs($user)->call('PUT', 'admin/labs/'.$lab->id, 
-	 	[ 
-	         'name' => str_random(10),
-	         'lab_code' => str_random(10),
-             'lab_init' => str_random(10) , 
-             'description' => str_random(10) ,
-	         'is_active' => mt_rand(0,1)
-	     ]);   
-	
-         $this->assertEquals(200, $response->status());
-	      
+		[ 
+			'name' => str_random(10),
+			'lab_code' => str_random(10),
+			'lab_init' => str_random(10) , 
+			'description' => str_random(10) ,
+			'is_active' => mt_rand(0,1)
+		
+		]);   
+		$this->assertEquals(302, $response->status());
 	}
-     public function test_delete()
-	 { 
-	 	$user = factory(App\User::class)->create(); 
-        $lab = ExaminationLab::latest()->first();
-	 	$response =  $this->actingAs($user)->call('DELETE', 'admin/labs/'.$lab->id);   
-         $this->assertEquals(200, $response->status());
-	     
-	}*/
+
+	public function test_edit()
+	{ 
+		$admin = User::find('1');
+		$response = $this->actingAs($admin)->call('GET', 'admin/labs/edit');  
+		$this->assertEquals(200, $response->status());
+	}
+
+	public function test_update()
+	{ 
+		$user = factory(App\User::class)->create(); 
+		$lab = ExaminationLab::latest()->first();
+		$response =  $this->actingAs($user)->call('PUT', 'admin/labs/'.$lab->id, 
+		[ 
+			'name' => str_random(10),
+			'lab_code' => str_random(10),
+			'lab_init' => str_random(10) , 
+			'description' => str_random(10) ,
+			'is_active' => mt_rand(0,1)
+		]);   
+		$this->assertEquals(200, $response->status());
+	}
+
+	public function test_delete()
+	{ 
+		$user = factory(App\User::class)->create(); 
+		$lab = ExaminationLab::latest()->first();
+		$response =  $this->actingAs($user)->call('DELETE', 'admin/labs/'.$lab->id);   
+		$this->assertEquals(200, $response->status());
+	}
 }
