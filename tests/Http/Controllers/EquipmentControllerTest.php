f6b73f51af0584f04872f7c8735d96785a460dfa
daniel_ubuntu <danielchristianto77@gmail.com>
2020-09-01 01:27:38 +0700

    Test EquipmentController

--- tests/Http/Controllers/EquipmentControllerTest.php
+++ tests/Http/Controllers/EquipmentControllerTest.php
@@ -4,78 +4,77 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Equipment;
+use App\EquipmentHistory;
 use App\User;
 class EquipmentControllerTest extends TestCase
 {
-    
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+	public function testIndexWithSearch()
+	{
+		
+		$this->actingAs(User::find('1'))->call('GET', 'admin/equipment?search=cari');  
+        $this->assertResponseStatus(200)->see('Penerimaan Barang');
+	}
 
-	public function testDeleteSoon(){
-        $this->assertTrue(true);
-    }
+	public function testCreate()
+	{
+		$this->actingAs(User::find('1'))->call('GET', 'admin/equipment/create');
+		$this->assertResponseStatus(200)->see('Tambah Barang Baru');
+	}
 
+	public function testStores()
+	{
+		$this->actingAs(User::find('1'))->call('POST', 'admin/equipment', [ 
+			'examination_id' => factory(App\Examination::class)->create()->id,
+			'name' => 'Nama equipment',
+			'qty' => array(1),
+			'unit' => array('unit uji device'),
+			'description' => array('deskripsi device'),
+			'pic' => 'Daniel',
+			'remarks' => array('The remarks'),
+			'equip_date' => Carbon\Carbon::now(),
+		]);
+		$this->assertRedirectedTo('/admin/equipment', ['message' => 'Equipment successfully created']);
+	}
 
-    public function testVisit()
-	 { 
-		$admin = User::find('1');
-		$response = $this->actingAs($admin)->call('GET', 'admin/equipment');
-        $this->assertEquals(200, $response->status());
-	 }
-	 public function test_search()
-	 { $admin = User::find('1');
-		$response = $this->actingAs($admin)->call('GET', 'admin/equipment?search=cari');  
-        $this->assertEquals(200, $response->status());
-	 }
-	 public function testCreate()
-	 { 
-		$admin = User::find('1');
-		$response = $this->actingAs($admin)->call('GET', 'admin/equipment/create');
-        $this->assertEquals(200, $response->status());
-	 }
+	public function testShow()
+	{
+		$equipmentHistory = factory(App\EquipmentHistory::class)->create();
+		$this->actingAs(User::find('1'))->call('GET', "admin/equipment/$equipmentHistory->examination_id");
+		$this->assertResponseStatus(200)->see('Detail Barang');
+	}
 
-	
-  /*   public function testStores()
-	 { 
-		$admin = User::find('1'); 
-		$examination = factory(App\Examination::class)->create();
-	 	$response =  $this->actingAs($admin)->call('POST', 'admin/equipment', 
-	 	[ 
-             'examination_id' => $examination->id,
-	         'name' => str_random(10),
-	         'qty' => mt_rand(0,10),
-	         'unit' => mt_rand(0,10) ,
-	         'description' =>  str_random(10),
-	         'pic' => str_random(10) ,
-	         'remark' =>mt_rand(0,1) 
-	     ]);   
-         $this->assertEquals(302, $response->status());
-	     
-		}
-     public function testUpdate()
-	 { 
-	 	$user = factory(App\User::class)->create(); 
-        $equipment = Equipment::latest()->first();
-	 	$response =  $this->actingAs($user)->call('PUT', 'admin/equipment/'.$equipment->id, 
-	 	[ 
-	         'examination_id' => str_random(10),
-	         'name' => str_random(10),
-	         'qty' => mt_rand(0,10),
-	         'unit' => mt_rand(0,10) ,
-	         'description' =>  str_random(10),
-	         'pic' => str_random(10) ,
-	         'remark' =>mt_rand(0,1) 
-	     ]);   
-         $this->assertEquals(302, $response->status());
-	 }
-     public function testDelete()
-	 { 
-	 	$user = factory(App\User::class)->create(); 
-        $company = Equipment::latest()->first();
-	 	$response =  $this->actingAs($user)->call('DELETE', 'admin/equipment/');  
-         $this->assertEquals(302, $response->status());  
-	 } */
+	public function testEdit()
+	{
+		$equipmentHistory = EquipmentHistory::latest()->first();
+		$this->actingAs(User::find('1'))->call('GET', "admin/equipment/$equipmentHistory->examination_id");
+		$this->assertResponseStatus(200)->see('Detail Barang');
+	}
+
+	public function testUpdate()
+	{
+		$equipmentHistory = EquipmentHistory::latest()->first();
+		$this->actingAs(User::find('1'))->call('PATCH', "admin/equipment/$equipmentHistory->examination_id", [
+			'location' => 1,
+			'pic' => 'Daniel',
+			'location_id' => 2,
+			'equip_date' => Carbon\Carbon::now(),
+
+			// 'examination_id' => factory(App\Examination::class)->create()->id,
+			// 'name' => 'Nama equipment',
+			// 'qty' => array(1),
+			// 'unit' => array('unit uji device'),
+			// 'description' => array('deskripsi device'),
+			// 'remarks' => array('The remarks'),
+			
+		]);
+		$this->assertRedirectedTo('/admin/equipment', ['message' => 'Equipment successfully updated']);
+	}
+
+	public function testDelete()
+	{
+		$equipmentHistory = EquipmentHistory::latest()->first();
+		$this->actingAs(User::find('1'))->call('DELETE','admin/equipment/'.$equipmentHistory->examination_id);
+		$this->assertRedirectedTo('/admin/equipment', ['message' => 'Equipment successfully deleted']);
+	}
+
 }
