8226964c99b85d263ad1e97343f0c64fc50741ac
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2020-09-02 00:00:27 +0700

    edit Handler to throw catch

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,7 +45,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        if(app()->environment('testing')){
+        if(App::environment('testing')){
             throw $e;
         }
 
