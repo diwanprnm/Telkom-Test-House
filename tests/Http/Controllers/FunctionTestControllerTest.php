ea32d5ecf52ed3219367a0156e0f1d17bf1620c3
daniel_ubuntu <danielchristianto77@gmail.com>
2020-09-03 19:52:29 +0700

    FunctionTestController fix and test

---
+++ tests/Http/Controllers/FunctionTestControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\User;
+
+class FunctionTestControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testIndex()
+    {
+        $this->actingAs(User::find('1'))->call('GET', 'admin/functiontest'); 
+        $this->assertResponseStatus(200)->see('Data Uji Fungsi');
+    }
+
+    public function testExcel()
+    {
+        factory(App\Examination::class)->create(['function_status' => 0]);
+        factory(App\Examination::class)->create(['function_status' => 0, 'function_date' => null]);
+        factory(App\Examination::class)->create(['function_status' => 0, 'function_test_TE' => 1]);
+        factory(App\Examination::class)->create(['function_status' => 0, 'function_test_TE' => 2]);
+        factory(App\Examination::class)->create(['function_status' => 0, 'function_test_TE' => 3]);
+        factory(App\Examination::class)->create(['function_status' => 0, 'function_test_TE' => 4]);
+
+        $this->actingAs(User::find('1'))->call('GET', 'admin/functiontest'); 
+        $this->assertResponseStatus(200);
+    }
+
+
+
+    
+}
