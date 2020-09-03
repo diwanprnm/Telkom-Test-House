becaf906298997375905364889fab0b13a3202ae
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2020-09-03 09:12:35 +0700

    reset TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Exceptions\Handler;
-use Illuminate\Contracts\Debug\ExceptionHandler;
-
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
     /**
@@ -24,22 +21,5 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
-    }
-
-    // Use this version if you're on PHP 7
-    protected function disableExceptionHandling()
-    {
-        $this->app->instance(ExceptionHandler::class, new class extends Handler {
-            public function __construct() {}
-            
-            public function report(Exception $e)
-            {
-                // no-op
-            }
-            
-            public function render($request, Exception $e) {
-                throw $e;
-            }
-        });
     }
 }
