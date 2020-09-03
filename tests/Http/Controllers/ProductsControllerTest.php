2215207cb969d5f6f4241547112ff9b06bad8e1d
darulfazhal <fazhaldarul@gmail.com>
2020-09-03 21:35:00 +0700

    Unit testing products + profile

--- tests/Http/Controllers/ProductsControllerTest.php
+++ tests/Http/Controllers/ProductsControllerTest.php
@@ -3,7 +3,8 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-
+use App\User; 
+use App\STELSales;
 class ProductsControllerTest extends TestCase
 {
     /**
@@ -11,8 +12,45 @@ class ProductsControllerTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+    public function test_visit()
+	{ 
+	   $user = User::find(1);
+	   $response =  $this->actingAs($user)->call('GET', '/products');  
+       $this->assertEquals(200, $response->status());
+	}
+ //    public function test_purchase_history()
+	// { 
+	//    $user = User::find(1);
+	//    $response =  $this->actingAs($user)->call('GET', '/purchase_history');  
+
+	   
+ //       $this->assertEquals(200, $response->status());
+	// }
+    public function test_payment_status()
+	{ 
+	   $user = User::find(1);
+	   $response =  $this->actingAs($user)->call('GET', '/payment_status');  
+	    
+       $this->assertEquals(200, $response->status());
+	}
+    public function test_payment_detail()
+	{ 
+	   $user = User::find(1);
+	   $stelsSales = factory(App\STELSales::class)->create(['id'=>'00000000-aaaa-aaaa-aaaa-000000000000']);
+	   $stelSalesDetail = factory(App\STELSalesDetail::class)->create(['stels_sales_id' => $stelsSales->id]);
+       $stels = App\STEL::find($stelSalesDetail->stels_id);
+	   $response =  $this->actingAs($user)->call('GET', '/payment_detail/'.$stelsSales->id);  
+	   
+       $this->assertEquals(200, $response->status());
+	}
+    public function test_upload_payment()
+	{ 
+	   $user = User::find(1);
+	   $stelsSales = factory(App\STELSales::class)->create(['id'=>'00000000-aaaa-aaaa-aaaa-000000000000']);
+	   $stelSalesDetail = factory(App\STELSalesDetail::class)->create(['stels_sales_id' => $stelsSales->id]);
+       $stels = App\STEL::find($stelSalesDetail->stels_id);
+	   $response =  $this->actingAs($user)->call('GET', '/upload_payment/'.$stelsSales->id);  
+	   
+       $this->assertEquals(200, $response->status());
+	}
 }
--- tests/Http/Controllers/ProfileControllerTest.php
+++ tests/Http/Controllers/ProfileControllerTest.php
@@ -3,7 +3,8 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-
+use App\User; 
+use App\Company; 
 class ProfileControllerTest extends TestCase
 {
     /**
@@ -11,8 +12,68 @@ class ProfileControllerTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
-    {
-        $this->assertTrue(true);
-    }
+    public function test_visit()
+	{ 
+	   $user = User::find(1);
+	   $response =  $this->actingAs($user)->call('GET', '/client/profile?tabs=company');  
+       $this->assertEquals(200, $response->status());
+	}
+    public function test_update_user()
+	{ 
+	    $user = User::find(1);  
+		$response =  $this->actingAs($user)->call('POST', '/client/profile', 
+		[ 
+	        'hide_id_user' => $user->id,  
+	        'username' => str_random(10),  
+	        'email' => str_random(10), 
+	        'address' => str_random(10), 
+	        'password' => str_random(10), 
+	        'phone_number' => 1, 
+	        'fax' => str_random(10), 
+	        'email2' => str_random(10), 
+	        'email3' => str_random(10) 
+	    ]);    
+	    
+        $this->assertEquals(302, $response->status()); 
+	}
+    public function test_update_company()
+	{ 
+	    $user = User::find(1);  
+	    $company = Company::find(1);  
+		$response =  $this->actingAs($user)->call('POST', '/client/company', 
+		[ 
+	        'hide_id_company' => $company->id,   
+	        'name' => str_random(10),   
+	        'address' => str_random(10), 
+	        'plg_id' => str_random(10), 
+	        'nib' => 1, 
+	        'city' => str_random(10), 
+	        'email' => str_random(10), 
+	        'postal_code' => str_random(10), 
+	        'phone' => str_random(10) , 
+	        'fax' => str_random(10) , 
+	        'npwp_number' => str_random(10), 
+	        'siup_number' => str_random(10) ,
+	        'certificate_number' => str_random(10) ,
+	    ]);    
+	    
+        $this->assertEquals(302, $response->status()); 
+	}
+    public function test_check_registrasi_email()
+	{ 
+	    $user = User::find(1);   
+		$response =  $this->actingAs($user)->call('POST', '/checkRegisterEmail', 
+		[  
+	        'email' => str_random(10) 
+	    ]);    
+	    
+        $this->assertEquals(200, $response->status()); 
+	}
+    public function test_register()
+	{ 
+	    $user = User::find(1);    
+		$response =  $this->actingAs($user)->call('GET', '/register');    
+	    
+        $this->assertEquals(302, $response->status()); 
+	}
 }
