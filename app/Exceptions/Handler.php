c634bb862592abde70b90506e1e8941dd7cc7e74
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2020-09-04 12:38:59 +0700

    comment sementara semua cetak PDF

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
+        if (env('APP_ENV') === 'testing') {
+            throw $e;
+        }
         return parent::render($request, $e);
     }
 }
