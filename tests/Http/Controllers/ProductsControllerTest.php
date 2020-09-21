c0484fb5aa947a130b19f4f83b6edb4bc0cf08f6
darulfazhal <fazhaldarul@gmail.com>
2020-09-21 21:43:35 +0700

    unit testing product & usereks

--- tests/Http/Controllers/ProductsControllerTest.php
+++ tests/Http/Controllers/ProductsControllerTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\User; 
-use App\STELSales;
+use App\STELSales;  
 class ProductsControllerTest extends TestCase
 {
     /**
@@ -59,4 +59,43 @@ class ProductsControllerTest extends TestCase
 	   
        $this->assertEquals(200, $response->status());
 	}
+    public function test_upload_pembayaranstel()
+	{   
+        $user = User::find(1); 
+ 
+        
+		$response =  $this->actingAs($user)->call('POST', '/pembayaranstel', 
+		[ 
+	        'jml-pembayaran' => mt_rand(0,10000), 
+	        'filePembayaran' => NULL, 
+	    ]);   
+		// dd($response->getContent());
+        $this->assertEquals(302, $response->status());  
+	}
+    public function test_store()
+	{   
+        $user = User::find(1); 
+ 	
+        
+		$response =  $this->actingAs($user)->call('POST', '/products', 
+		[ 
+	        'id' => mt_rand(0,10000), 
+	        'name' => str_random(10), 
+	        'qty' => 1, 
+	        'price' => 1
+	    ]);   
+		// dd($response->getContent());
+        $this->assertEquals(302, $response->status());  
+	}
+    public function test_checkout()
+	{   
+        $user = User::where('role_id', '=', '2')->first();
+		$response =  $this->actingAs($user)->call('POST', '/checkout', 
+		[ 
+	        'agree' => TRUE 
+	    ]);   
+		// dd($response->getContent());
+        $this->assertEquals(200, $response->status());  
+	}
 }
+	
