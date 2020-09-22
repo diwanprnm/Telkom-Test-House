884314f33da11e7194cbbf55aebca01d6caa5657
darulfazhal <fazhaldarul@gmail.com>
2020-09-22 09:36:47 +0700

    testing usereks

--- tests/Http/Controllers/UsereksControllerTest.php
+++ tests/Http/Controllers/UsereksControllerTest.php
@@ -38,25 +38,25 @@ class UsereksControllerTest extends TestCase
 		$this->assertEquals(200, $response->status());
 	}
 
-	// public function test_stores_user()
-	// { 
-	// 	$admin = User::find('1');
+	public function test_stores_user()
+	{ 
+		$admin = User::find('1');
 		   
-	// 	$response = $this->actingAs($admin)->call('POST', 'admin/usereks', 
-	// 	[ 
-	//         'name' => str_random(10), 
-	//         'role_id' => 2, 
-	//         'company_id' => 1, 
-	//         'email' => str_random(10), 
-	//         'password' => str_random(10), 
-	//         'is_active' => 1, 
-	//         'address' => str_random(10), 
-	//         'phone_number' => str_random(10), 
-	//         'fax' => str_random(10)
-	//     ]);    
+		$response = $this->actingAs($admin)->call('POST', 'admin/usereks', 
+		[ 
+	        'name' => str_random(10), 
+	        'role_id' => 2, 
+	        'company_id' => 1, 
+	        'email' => str_random(10), 
+	        'password' => str_random(10), 
+	        'is_active' => 1, 
+	        'address' => str_random(10), 
+	        'phone_number' => str_random(10), 
+	        'fax' => str_random(10)
+	    ]);    
 
-	// 	$this->assertEquals(302, $response->status()); 
-	// }
+		$this->assertEquals(302, $response->status()); 
+	}
 
 	public function testShow()
 	{
@@ -67,50 +67,50 @@ class UsereksControllerTest extends TestCase
 		$this->assertEquals(200, $response->status());
 	}
 
-	// public function test_visit_edit_usereks()
-	// { 
-	// 	$admin = User::find('1');
-	// 	$user = User::latest()->first(); 
-	//    	$response =  $this->actingAs($admin)->call('GET', 'admin/usereks/'.$user->id.'/edit');  
+	public function test_visit_edit_usereks()
+	{ 
+		$admin = User::find('1');
+		$user = User::latest()->first(); 
+	   	$response =  $this->actingAs($admin)->call('GET', 'admin/usereks/'.$user->id.'/edit');  
 	    
- //       	$this->assertEquals(200, $response->status());
-	// }
+       	$this->assertEquals(200, $response->status());
+	}
 
- //    public function test_update_usereks()
-	// { 
-	// 	$admin = User::find('1');
-	// 	$user = User::latest()->first(); 
-	// 	$response =  $this->actingAs($admin)->call('PUT', 'admin/usereks/'.$user->id, 
-	// 	[ 
-	//         'name' => str_random(10), 
-	//         'role_id' => 2, 
-	//         'company_id' => 0, 
-	//         'email' => str_random(10), 
-	//         'password' => str_random(10), 
-	//         'is_active' => 1, 
-	//         'address' => str_random(10), 
-	//         'phone_number' => str_random(10), 
-	//         'fax' => str_random(10),
-	//         'hide_admin_role' =>1,
-	//     ]);    
+    public function test_update_usereks()
+	{ 
+		$admin = User::find('1');
+		$user = User::latest()->first(); 
+		$response =  $this->actingAs($admin)->call('PUT', 'admin/usereks/'.$user->id, 
+		[ 
+	        'name' => str_random(10), 
+	        'role_id' => 2, 
+	        'company_id' => 0, 
+	        'email' => str_random(10), 
+	        'password' => str_random(10), 
+	        'is_active' => 1, 
+	        'address' => str_random(10), 
+	        'phone_number' => str_random(10), 
+	        'fax' => str_random(10),
+	        'hide_admin_role' =>1,
+	    ]);    
 	    
- //        $this->assertEquals(302, $response->status());  
-	// }
+        $this->assertEquals(302, $response->status());  
+	}
 
-	// public function test_softdelete_usereks()
-	// { 
-	// 	$admin = User::find('1');
-	// 	$user = User::latest()->first(); 
-	// 	$response = $this->actingAs($admin)->call('POST', 'admin/usereks/'.$user->id.'/softDelete');  
- //        $this->assertEquals(302, $response->status()); 
-	// } 
+	public function test_softdelete_usereks()
+	{ 
+		$admin = User::find('1');
+		$user = User::latest()->first(); 
+		$response = $this->actingAs($admin)->call('POST', 'admin/usereks/'.$user->id.'/softDelete');  
+        $this->assertEquals(302, $response->status()); 
+	} 
 
- //    public function test_delete_usereks()
-	// { 
-	// 	$admin = User::find('1');
-	// 	$user = User::latest()->first(); 
-	// 	$response = $this->actingAs($admin)->call('DELETE', 'admin/usereks/'.$user->id); 
-	// 	// dd($response->getContent()); 
- //        $this->assertEquals(302, $response->status()); 
-	// } 
+    public function test_delete_usereks()
+	{ 
+		$admin = User::find('1');
+		$user = User::latest()->first(); 
+		$response = $this->actingAs($admin)->call('DELETE', 'admin/usereks/'.$user->id); 
+		// dd($response->getContent()); 
+        $this->assertEquals(302, $response->status()); 
+	} 
 }
