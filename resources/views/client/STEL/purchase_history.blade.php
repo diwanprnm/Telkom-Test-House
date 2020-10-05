83e59ef125451092a0a61de9fde604431bde00f6
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2020-10-05 17:16:26 +0700

    error payment_status

--- resources/views/client/STEL/purchase_history.blade.php
+++ resources/views/client/STEL/purchase_history.blade.php
@@ -79,6 +79,7 @@
 															break; 
 														case 2:
 															echo '<td class="center"><span class="label label-sm label-warning" style="line-height: 2;">Paid (waiting confirmation)</span></td>';
+															break;
 														case 3:
 															echo '<td class="center"><span class="label label-sm label-info" style="line-height: 2;">Paid (delivered)</span></td>';
 															break; 
