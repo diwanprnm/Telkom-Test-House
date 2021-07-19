1c6a219edc39a0d6da7765d814cd55218ce1af5f
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2021-07-19 20:07:14 +0700

    test push

--- database/migrations/2021_07_01_092239_add_columns_to_stels_table.php
+++ database/migrations/2021_07_01_092239_add_columns_to_stels_table.php
@@ -14,7 +14,7 @@ class AddColumnsToStelsTable extends Migration
     {
         Schema::table('stels', function (Blueprint $table) {
             $table->date('publish_date')->nullable();
-            $table->uuid('stels_master_id');
+            $table->uuid('stels_master_id')->nullable();
 
             // $table->foreign('stels_master_id')->references('id')->on('stels_master');
         });
