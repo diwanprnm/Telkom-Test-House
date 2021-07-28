7f9f4efef11b7a25e03b8ac15f6a4b2b004c2945
setiadijoe <setiadi_y56@yahoo.co.id>
2021-07-28 21:24:11 +0700

    add preview button

--- resources/views/admin/email_editors/edit.blade.php
+++ resources/views/admin/email_editors/edit.blade.php
@@ -1,6 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
+
+<style>
+
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+  font-size: 17px;
+}
+
+/* Change background color of buttons on hover */
+.btn:hover {
+  background-color: rgb(219, 171, 67);
+}
+
+/* Create an active/current tablink class */
+.btn.active {
+  background-color: rgb(0, 46, 253);
+}
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
+}
+</style>
 <div class="main-content" >
 	<div class="wrap-content container" id="container">
 		<!-- start: PAGE TITLE -->
@@ -62,10 +92,17 @@
 									<label>
 										Konten/Isi *
 									</label>
-									<?php 
-										$content = str_replace('&', '&amp;', $data->content);
-									?>
-									<textarea type="text" id="content" name="content" class="form-control" placeholder="Konten Email ..."><?= $content ?></textarea>
+									<ul class="tabs">
+										<li class="btn tab-1 tablinks" data-tab="tab-1" id="defaultOpen" onclick="openTab(event, 'Input')">Write</li>
+										<li class="btn tab-2 tablinks" data-tab="tab-2" id="previewtab" onclick="openTab(event, 'Preview');updateFields()">Preview</li>
+									</ul>
+
+									<div id="Input" class="tabcontent">
+										<textarea type="text" id="content-block" name="content" style="display: block" class="form-control" placeholder="Konten Email ..."><?= str_replace('&', '&amp;', $data->content) ?></textarea>
+									</div>
+									<div id="Preview" class="tabcontent">
+
+									</div>
 								</div>
 							</div>
 	                        <div class="col-md-12">
@@ -100,18 +137,44 @@
 <script src={{ asset("vendor/jquery-validation/jquery.validate.min.js") }}></script>
 <script src={{ asset("assets/js/form-elements.js") }}></script>
 <script src="https://cdn.ckeditor.com/ckeditor5/28.0.0/classic/ckeditor.js"></script>
+<script type="text/javascript" src="ckeditor/ckeditor.js"></script>
 <script type="text/javascript">
 	jQuery(document).ready(function() {
 		FormElements.init();
 	});
+
+	var myContent
 	ClassicEditor
-		.create(document.querySelector('#content'))
+		.create(document.querySelector('#content-block'))
 		.then(content => {
-			console.log("ini isi contentnya");
-			console.log(content.getData());
+			myContent = content
 		})
 		.catch(err => {
 			console.log(err);
 		});
+
+	
+	function updateFields() {	
+		var value = myContent.getData();
+		document.getElementById('Preview').innerHTML = value;
+	}
+</script>
+<script>
+	function openTab(evt, tab) {
+	  var i, tabcontent, tablinks;
+	  tabcontent = document.getElementsByClassName("tabcontent");
+	  for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	  }
+	  tablinks = document.getElementsByClassName("tablinks");
+	  for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	  }
+	  document.getElementById(tab).style.display = "block";
+	  evt.currentTarget.className += " active";
+	}
+
+	document.getElementById("defaultOpen").click();
+
 </script>
 @endsection
