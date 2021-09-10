923ce035d25dbd2e5c1deffb829313dff78ef025
Arif Chandra Simanjuntak <arifchandrasimanjuntak@gmail.com>
2021-09-10 16:03:34 +0700

    Gajadi

--- resources/views/client/pengujian/index.blade.php
+++ resources/views/client/pengujian/index.blade.php
@@ -4,7 +4,7 @@
 @extends('layouts.client')
 <!-- Document Title
     ============================================= -->
-    <title>{{ trans('translate.examination') }} - Telkom DDBZ</title>
+    <title>{{ trans('translate.examination') }} - Telkom DDB</title>
 @php $date_STRING = date('d-m-Y'); @endphp
 @section('content')
 <style type="text/css">
