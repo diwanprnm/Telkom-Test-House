c09ebcd12871d231c23c61645fc89e827f1fe10d
danielchwi <daniel.widodo@bitcoin.co.id>
2021-09-22 22:12:44 +0700

    Add dummy data testing & multiline capability handling

--- app/Services/PDF/CetakSidangQA.php
+++ app/Services/PDF/CetakSidangQA.php
@@ -10,8 +10,8 @@ class CetakSidangQA
     public function makePDF($data, $pdf)
     {
 
-        //bussiners process - logic
-        $numberOfPages = 2;//(int)ceil(count($data["sidang_detail"])/4);
+        // BUSSINESS PROCESS, LOGIC, & CONFIG
+        $numberOfPages = (int)ceil(count($data["sidang_detail"])/4);
         $year = substr($data["sidang_detail"][0]->valid_from,0,4);
         $data['date'] = \App\Services\MyHelper::tanggalIndonesia($data["sidang"]->date);
         $method = $data['method'] ?? '';
@@ -23,6 +23,25 @@ class CetakSidangQA
             0 => 'Belum',
             1 => 'Lulus',
             2 => 'Pending',
+        ];
+        $dataDummy = false;
+        $dummy = [
+            [
+                'started' => '2021-06-15',
+                'finished' => '2021-07-22',
+                'target' => '2021-07-23',
+                'hasil' => 'COMPLY',
+                'result' => 1,
+                'note' => 'Suspendisse diam nunc, molestie non cursus a, dignissim at lectus. Etiam in erat urna. In condimentum felis ac metus interdum, ut sollicitudin ex blandit. Nullam vulputate et neque at facilisis'
+            ],
+            [
+                'started' => '2021-06-28',
+                'finished' => '2021-07-23',
+                'target' => '2021-08-10',
+                'hasil' => 'NOT COMPLY',
+                'result' => 2,
+                'note' => 'Aliquam tristique felis sit amet sem placerat tristique. Donec sodales neque a vestibulum dapibus. Suspendisse convallis ligula quis ipsum condimentum aliquam. Nulla mattis velit non mi vestibulum, at finibus tellus pharetra'
+            ]
         ];
         
         // PDF CONFIG
@@ -53,7 +72,6 @@ class CetakSidangQA
             $pdf->Cell(57,$rowHeight,'No. SPK',1,1,'R',false);
             $pdf->Cell(57,$rowHeight,'No. Laporan',1,1,'R',false);$pdf->SetFont('helvetica','B',9);
             $pdf->Cell(57,$rowHeight,'No. Sertifikat',1,1,'R',false);$pdf->SetFont('helvetica','',9);
-            //$pdf->Cell(57,$rowHeight,'Expired CIQS',1,1,'R',false);
             $pdf->Cell(57,$rowHeight*2,'PEMOHON/Company',1,1,'R',false);
             $pdf->Cell(57,$rowHeight*2,'PERANGKAT/Equipment',1,1,'R',false);
             $pdf->Cell(57,$rowHeight*2,'MEREK/Brand',1,1,'R',false);
@@ -74,27 +92,41 @@ class CetakSidangQA
 
             /*TABLE BODY CONTENT*/
             $pdf->SetFont('helvetica','',8);
-            for ($i=0 + ($n*4) ; $i < (($n+1)*4); $i++) {
+            for ($i=0 + ($n*4) ; $i < (($n+1)*4); $i++){
+                $dataNumber = $dataDummy ? $i%2 : $i;
+                
                 $pdf->setXY(67+(($i%4)*55),30+$rowHeight);
-                $pdf->Cell(55,$rowHeight,$data['sidang_detail'][$i]->examination->spk_code??'',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,$i<$dataCount ? ($data['sidang_detail'][$i]->examination->media->where('name', 'Laporan Uji')->first()->no??'') : '',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,$data['certificateNumber'][$i]??'',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight*2,$data['sidang_detail'][$i]->examination->company->name??'',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight*2,$data['devices'][$i]->name??'',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight*2,$data['devices'][$i]->mark??'',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,$data['devices'][$i]->model??'',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,$data['devices'][$i]->capacity??'',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,$data['devices'][$i]->serial_number??'',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,$data['devices'][$i]->test_reference??'',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,$data['devices'][$i]->manufactured_by??'',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight, $i<$dataCount ? \App\Services\MyHelper::tanggalIndonesia( $data['sidang_detail'][$i]->examination->equipmentHistory->where('location', 2)->first()->action_date ?? '' ) ?? '' : '' ,1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,\App\Services\MyHelper::tanggalIndonesia($data['sidang_detail'][$i]->startDate ?? '') ?? '',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,\App\Services\MyHelper::tanggalIndonesia($data['sidang_detail'][$i]->endDate ?? '') ?? '',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,$i<$dataCount ? $data['sidang_detail'][$i]->examination->examinationLab->name ?? '' : '',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,\App\Services\MyHelper::tanggalIndonesia($data['sidang_detail'][$i]->targetDate ?? '') ?? '',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,$data['sidang_detail'][$i]->finalResult ?? '',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight*5,$data['sidang_detail'][$i]->catatan ?? '',1,2,'C',false);
-                $pdf->Cell(55,$rowHeight,$i<$dataCount ? $listKeputusanSidangQA[$data['sidang_detail'][$i]->finalResult ?? ''] : '',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight,$data['sidang_detail'][$dataNumber]->examination->spk_code??'',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight, $i<$dataCount || $dataDummy ? ($data['sidang_detail'][$dataNumber]->examination->media->where('name', 'Laporan Uji')->first()->no??'') : '',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight,$data['certificateNumber'][$dataNumber]??'',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight*2,'',1,2,'',false); //company_name multiline (handle diferently)
+                $pdf->Cell(55,$rowHeight*2,'',1,2,'',false); //device_name multiline (handle diferently)
+                $pdf->Cell(55,$rowHeight*2,'',1,2,'',false); //device_mark multiline (handle diferently)
+                $pdf->Cell(55,$rowHeight,$data['devices'][$dataNumber]->model??'',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight,$data['devices'][$dataNumber]->capacity??'',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight,$data['devices'][$dataNumber]->serial_number??'',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight,$data['devices'][$dataNumber]->test_reference??'',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight,$data['devices'][$dataNumber]->manufactured_by??'',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight, $i<$dataCount || $dataDummy ? \App\Services\MyHelper::tanggalIndonesia( $data['sidang_detail'][$dataNumber]->examination->equipmentHistory->where('location', 2)->first()->action_date ?? '' ) ?? '' : '' ,1,2,'C',false);
+                $pdf->Cell(55,$rowHeight, $dataDummy ? \App\Services\MyHelper::tanggalIndonesia($dummy[$dataNumber]['started']) : \App\Services\MyHelper::tanggalIndonesia($data['sidang_detail'][$dataNumber]->startDate ?? '') ?? '',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight, $dataDummy ? \App\Services\MyHelper::tanggalIndonesia($dummy[$dataNumber]['finished']) : \App\Services\MyHelper::tanggalIndonesia($data['sidang_detail'][$dataNumber]->endDate ?? '') ?? '',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight, $i<$dataCount || $dataDummy ? $data['sidang_detail'][$dataNumber]->examination->examinationLab->name ?? '' : '',1,2,'C',false);
+                $pdf->Cell(55,$rowHeight, $dataDummy ? \App\Services\MyHelper::tanggalIndonesia($dummy[$dataNumber]['target']) : \App\Services\MyHelper::tanggalIndonesia($data['sidang_detail'][$dataNumber]->targetDate ?? '') ?? '',1,2,'C',false);$pdf->SetFont('helvetica','B',8);
+                $pdf->Cell(55,$rowHeight,$dataDummy ? $dummy[$dataNumber]['hasil'] : $data['sidang_detail'][$dataNumber]->finalResult ?? '',1,2,'C',false);$pdf->SetFont('helvetica','',8);
+                $pdf->Cell(55,$rowHeight*5,$data['sidang_detail'][$dataNumber]->catatan ?? '',1,2,'C',false);$pdf->SetFont('helvetica','B',8);
+                $pdf->Cell(55,$rowHeight, $i<$dataCount || $dataDummy ? $listKeputusanSidangQA[ ($dataDummy ? $dummy[$dataNumber]['result'] : $data['sidang_detail'][$dataNumber]->finalResult) ?? ''] : '',1,2,'C',false);$pdf->SetFont('helvetica','',8);
+
+                // Multiline Handling
+                if ($i<$dataCount || true){
+                    $pdf->setXY(67+(($i%4)*55),30+($rowHeight*4));
+                    $pdf->drawTextBox($data['sidang_detail'][$dataNumber]->examination->company->name??'', 55, 11, 'C', 'M');
+                    $pdf->setXY(67+(($i%4)*55),30+($rowHeight*6));
+                    $pdf->drawTextBox($data['devices'][$dataNumber]->name??'', 55, 11, 'C', 'M');
+                    $pdf->setXY(67+(($i%4)*55),30+($rowHeight*8));
+                    $pdf->drawTextBox($data['devices'][$dataNumber]->mark??'', 55, 11, 'C', 'M');
+                    $pdf->setXY(67+(($i%4)*55),30+($rowHeight*21));
+                    $pdf->drawTextBox($dataDummy ? $dummy[$dataNumber]['note'] : $data['sidang_detail'][$dataNumber]->catatan ?? '', 55, 27.5, 'C', 'M');
+                }            
             }
         }
 
